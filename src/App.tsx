49bf4094bc4ff9b788a943092f25a237216fca99
Jackson Socolofsky <jtsoco@gmail.com>
2026-02-21 19:20:12 +0900

    removed unnecessary things

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,12 @@
 import { useState, useRef} from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
-import { IntroductionSection } from './sections/introductionSection'
 import { AboutSection } from './sections/aboutSection'
 import { LanguageContext } from './contexts/languageContext'
 import { EnglishText, JapaneseText} from './assets/text'
 import { GameSection } from './sections/gameSection'
 
 function App() {
-  const [count, setCount] = useState(0)
   const [userLanguage, setUserLanguage] = useState<'en' | 'jp'>('en');
   const targetRef = useRef(null);
 
