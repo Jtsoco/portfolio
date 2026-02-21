18a4e2efa017d42eb1a0fbc38e8d88e3135d0951
Jackson Socolofsky <jtsoco@gmail.com>
2026-02-22 06:03:19 +0900

    moved images to public, added path correctly for deploy build version

--- src/assets/carouselItemInfo/pyxelGameCarousel.tsx
+++ src/assets/carouselItemInfo/pyxelGameCarousel.tsx
@@ -8,19 +8,19 @@
 export function pyxelGameInfo() {
   return [
     {
-      src: "public/games/platformer.gif",
+      src: "games/platformer.gif",
       alt: "Platformer Game Screenshot",
       captionTitle: "Pyxel Dungeon Explorer",
       captionText: "A retro-style dungeon exploration game built with Pyxel. Defeat bosses, get new weapons, shields, and powerups as you progress"
     },
     {
-      src: "public/games/boss.gif",
+      src: "games/boss.gif",
       alt: "Boss Fight Screenshot",
       captionTitle: "Boss Fights",
       captionText: "Beating a boss can even earn powerups from that boss."
     },
     {
-      src: "public/games/pyxel-title.png",
+      src: "games/pyxel-title.png",
       alt: "Title Screen Screenshot",
       captionTitle: "Title Screen",
       captionText: "The title screen of the game, with the ability to select a different starter character each with their own unique weapon and shield."
