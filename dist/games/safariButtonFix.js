382971e22ffd1c6c24aa015ed5c036ac989df2ae
Jackson Socolofsky <jtsoco@gmail.com>
2026-04-08 14:21:29 +0900

    built

--- dist/games/safariButtonFix.js
+++ dist/games/safariButtonFix.js
@@ -1,39 +1,35 @@
-
-(function() {
-  const arrowCodes = {
-    ArrowUp:   38,
-    ArrowDown: 40,
-    ArrowLeft: 37,
-    ArrowRight:39,
-  };
+function safariButtonFix() {
 
   function makeSynthetic(e) {
     return new KeyboardEvent(e.type, {
-      // key:       e.key,
-      code:      e.code,
-      // keyCode:   arrowCodes[e.key],
-      // which:     arrowCodes[e.key],
-      bubbles:   true,
-      // cancelable: true
+    key: e.key,
+    code: e.code,
+    location: 0,
+    keyCode: e.keyCode,
+    which: e.which,
+    repeat: e.repeat,
+    isComposing: e.isComposing,
+    ctrlKey: e.ctrlKey,
+    shiftKey: e.shiftKey,
+    altKey: e.altKey,
+    metaKey: e.metaKey,
+    bubbles: e.bubbles,
+    cancelable: e.cancelable,
+    composed: e.composed,
     });
   }
 
   function forwardArrow(e) {
-    if ( !e.key.startsWith("Arrow")) return;
     if (!e.isTrusted || !e.key.startsWith("Arrow")) return;
     const c = document.getElementById("canvas");
-    console.log('event is:', e, 'activeElement is:', document.activeElement);
     if (c && document.activeElement === c) {
       // for pyxel don't actually need to prevent default, it already won't scroll
-      // e.preventDefault();
-      c.dispatchEvent(makeSynthetic(e));
+      // and if not dispatching to the window where the listener is, bubbles needs to be true
+      window.dispatchEvent(makeSynthetic(e));
     }
   }
 
   function patchCanvas(c) {
-
-    console.log('patching canvas', c);
-
     c.tabIndex = -1;
 
     document.addEventListener("keydown", forwardArrow);
@@ -41,7 +37,6 @@
   }
 
 
-
-
   window.addEventListener("load", () => patchCanvas(document.getElementById("canvas")));
-})();
+};
+safariButtonFix();
